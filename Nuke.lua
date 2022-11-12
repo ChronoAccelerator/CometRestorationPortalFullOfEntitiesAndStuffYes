559cfc9978ffc7a58edd04f259ac1169c867333c
Chronos <112827733+ChronoAccelerator@users.noreply.github.com>
2022-11-12 07:55:20 -0800

    Update Nuke.lua

--- Nuke.lua
+++ Nuke.lua
@@ -8,7 +8,7 @@ game.Lighting.ColorCorrection.Contrast = 1
 local tween = game:GetService("TweenService")
 tween:Create(game.Lighting.ColorCorrection, TweenInfo.new(2.5), {Contrast = 0}):Play()
 local TweenService = game:GetService("TweenService")
-local TW = TweenService:Create(game.Lighting.MainColorCorrection, TweenInfo.new(3),{TintColor = Color3.fromRGB(255, 255, 255)})
+local TW = TweenService:Create(game.Lighting.ColorCorrection, TweenInfo.new(3),{TintColor = Color3.fromRGB(255, 255, 255)})
 TW:Play()
 for i,v in pairs(workspace:GetDescendants()) do
 if v:IsA("BasePart") then
